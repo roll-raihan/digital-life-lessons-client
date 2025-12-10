a1d5b19e66e438f6623308449c712b487d29e9c0
roll-raihan <raihanahmednasim@gmail.com>
2025-12-11 00:08:15 +0600

    added location to redirect page

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from "react-hook-form"
-import { Link } from 'react-router';
+import { Link, useLocation, useNavigate } from 'react-router';
 import useAuth from '../../hooks/useAuth';
 import { toast, ToastContainer } from 'react-toastify';
 import Swal from 'sweetalert2';
@@ -8,6 +8,8 @@ import axios from 'axios';
 
 const Register = () => {
 
+    const location = useLocation();
+    const navigate = useNavigate();
     const { registerUser, signInWithGoogle, updateUserProfile } = useAuth();
     const { register,
         handleSubmit, formState: { errors } } = useForm();
@@ -33,6 +35,7 @@ const Register = () => {
                         updateUserProfile(updateProfile)
                             .then(() => {
                                 // console.log('profile updated')
+                                navigate(location?.state || '/')
                             })
                             .catch(error => toast(error.message))
                     })
@@ -104,7 +107,7 @@ const Register = () => {
 
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-secondary mt-4">Register</button>
-                    <p>Already have an account? Please <Link className='text-blue-600 underline' to='/login'>Login</Link></p>
+                    <p>Already have an account? Please <Link state={location?.state} className='text-blue-600 underline' to='/login'>Login</Link></p>
                     <p className='font-bold text-center my-2'>Or,</p>
                     <button onClick={handleGoogleSignIn} className="btn btn-accent text-black border-[#e5e5e5]">
                         <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import useAuth from '../hooks/useAuth';
 import Loading from '../components/shared/Loading';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 
 const PrivateRoute = ({ children }) => {
 
     const { user, loading } = useAuth();
+    const location = useLocation();
 
     if (loading) {
         return <Loading></Loading>
     }
 
     if (!user) {
-        return <Navigate to='/login'></Navigate>
+        return <Navigate state={location?.pathname} to='/login'></Navigate>
     }
 
     return children;
