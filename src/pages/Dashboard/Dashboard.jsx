82c9a53fa125f7af0a3597df060ca2291375fa6d
roll-raihan <raihanahmednasim@gmail.com>
2025-12-12 11:34:47 +0600

    dashboard sidebar modified

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,14 +8,6 @@ const Dashboard = () => {
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                {/* Navbar */}
-                <nav className="navbar w-full bg-base-300">
-                    <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                        {/* Sidebar toggle icon */}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
-                    </label>
-                    <div className="px-4 font-bold text-2xl">Dashboard</div>
-                </nav>
                 <div><Outlet></Outlet></div>
             </div>
 
@@ -24,6 +16,13 @@ const Dashboard = () => {
                 <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
                     {/* Sidebar content here */}
                     <ul className="menu w-full grow">
+                        <li>
+                            {/* Sidebar toggle icon */}
+                                <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                    {/* Sidebar toggle icon */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+                                </label>
+                        </li>
                         {/* List item */}
                         <li>
                             <Link to='/' className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
@@ -40,7 +39,7 @@ const Dashboard = () => {
                                 <span className="is-drawer-close:hidden">Add Lessons</span>
                             </Link>
                         </li>
-                        
+
                         {/* my lessons */}
                         <li>
                             <Link to='/dashboard/my-lessons' className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="My Lessons">
